e5dbbe5285764db63ba473c07bcd6b55bccec7e2
Melquisedeque Pereira <melqui1@live.com.pt>
2019-11-01 08:44:15 +0000

    Imported required routes

--- index.js
+++ index.js
@@ -20,12 +20,15 @@ const cors = require('cors');
 
 const server = express();
 
+const projectsRouter = require('./projects/projectsRouter');
+const actionsRouter = require('./actions/actionsRouter');
+
 server.use(helmet());
 server.use(logger);
 server.use(express.json());
 server.use(cors());
-server.use('/api/projects', './projects/projectsRouter');
-server.use('/api/actions', './actions/actionsRouter');
+server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: "https://www.youtube.com/watch?v=d-diB65scQU" });
